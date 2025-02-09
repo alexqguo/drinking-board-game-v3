98003547c0fa2598a365ad384a664a0796e10ff3
Alex Guo <alexqguo@gmail.com>
2025-02-09 16:30:55 -0500

    fix current player not being set on game start

--- packages/engine/src/gamestate/gameStart.ts
+++ packages/engine/src/gamestate/gameStart.ts
@@ -5,6 +5,13 @@ import { GameState } from '../enums.js';
 
 export const GameStart: GameStateHandlerFactory = (ctx: BaseContext) => ({
   execute: () => {
+    // todo- make nicer
+    const firstPlayer = Object.values(ctx.nextGame.players).find(p => p.order === 0)!;
+
+    ctx.updateGameMetadata({
+      ...ctx.nextGame.metadata,
+      currentPlayerId: firstPlayer?.id
+    })
     return findGameStateHandler(ctx, GameState.TurnCheck).execute();
   },
   gameState: GameState.GameStart,
