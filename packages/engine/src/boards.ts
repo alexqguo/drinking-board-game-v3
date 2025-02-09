2dac932fcab1771643af0bc617672d1b87681501
Alex Guo <alexqguo@gmail.com>
2025-02-09 16:16:34 -0500

    Remove unused class

--- packages/engine/src/boards.ts
+++ packages/engine/src/boards.ts
@@ -14,12 +14,3 @@ export const hasBoard = (name: string): boolean => {
     return false;
   }
 };
-
-export class DecoratedBoard {
-  board: BoardModule;
-
-  constructor(board: BoardModule) {
-    this.board = board;
-    
-  }
-}
