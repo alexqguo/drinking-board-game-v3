8a8ae965fa991d845dbcdcc9571d2342f57c7f4d
Alex Guo <alexqguo@gmail.com>
2025-03-01 16:50:42 -0500

    Make cli a bit better

--- apps/cli/src/cli.ts
+++ apps/cli/src/cli.ts
@@ -1,5 +1,5 @@
+import { select } from '@inquirer/prompts';
 import { ActionType, BaseAction, BoardName, Game, PromptAction, requestHandler } from '@repo/engine';
-import readline from 'node:readline';
 
 // TODO - use commander and inquirer
 
@@ -10,50 +10,63 @@ const testLoggers = {
   error: console.error,
 }
 
-const askQuestion = (question: string): Promise<string> => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+// const askQuestion = (question: string): Promise<string> => {
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//   });
 
-  return new Promise((resolve) => {
-    rl.question(question, (answer) => {
-      rl.close();
-      resolve(answer);
-    });
+//   return new Promise((resolve) => {
+//     rl.question(question, (answer) => {
+//       rl.close();
+//       resolve(answer);
+//     });
+//   });
+// }
+
+let game: Game;
+
+const createGame = async () => {
+  const boardNames = Object.values(BoardName);
+  const boardName = await select({
+    message: 'Select a board',
+    choices: boardNames.map(n => ({
+      name: n,
+      value: n,
+    }))
   });
+  game = requestHandler({
+    action: ActionType.gameCreate,
+    actionArgs: {
+      playerNames: ['PLAYER1', 'PLAYER2'],
+      board: boardName
+    },
+    prevGame: null,
+    loggers: testLoggers
+  }).game;
+
+  game = requestHandler({
+    action: ActionType.gameStart,
+    prevGame: game,
+    actionArgs: {},
+    loggers: testLoggers
+  }).game;
+
+  gameLoop();
 }
 
-let testGame: Game = requestHandler({
-  action: ActionType.gameCreate,
-  actionArgs: {
-    playerNames: ['PLAYER1', 'PLAYER2'],
-    board: BoardName.Zelda
-  },
-  prevGame: null,
-  loggers: testLoggers
-}).game;
 
-testGame = requestHandler({
-  action: ActionType.gameStart,
-  prevGame: testGame,
-  actionArgs: {},
-  loggers: testLoggers
-}).game;
-
-async function main() {
+const gameLoop = async () => {
   while (true) {
-    // console.dir(testGame, { depth: null });
-
     const allActions: {
       pid: string,
       action: BaseAction,
     }[] = [];
 
-    Object.keys(testGame.availableActions).forEach(pid => {
+    Object.keys(game.availableActions).forEach(pid => {
       const actionsForPlayer = [
-        ...testGame.availableActions[pid]?.promptActions || [],
-        ...testGame.availableActions[pid]?.turnActions || [],
+        ...game.availableActions[pid]?.promptActions || [],
+        ...game.availableActions[pid]?.turnActions || [],
       ];
       actionsForPlayer.forEach(a => {
         allActions.push({
@@ -66,57 +79,46 @@ async function main() {
     // TODO- print out player statuses
     // TODO- print out prompt nicely if it exists
 
-    console.log('\nAvailable actions:');
-    allActions.forEach((a, idx) => {
-      console.log(`[${idx}] | ${testGame.players[a.pid]?.name} | action:${a.action.type}`);
-      if (a.action.result) {
-        console.log(`✅ - ${a.action.result}\n`);
-        return;
-      }
-      if ((a.action as PromptAction).candidateIds) {
-        console.log(`Options: ${(a.action as PromptAction).candidateIds}`)
-      }
-      console.log('\n');
+    const userActionIdx = await select({
+      message: 'What action do you want to take?',
+      choices: allActions.map((a, idx) => ({
+        name: a.action.result ? `✅ - ${a.action.result}`
+          : `[${game.players[a.pid]?.name}] | action:${a.action.type}\n${(a.action as PromptAction).candidateIds}`,
+        value: idx,
+        disabled: !!a.action.result
+      })),
     });
 
-    const userAction = await askQuestion("What # action do you want to take? (<actionIdx> <optionIdx>) ");
+    let userCandidateId = null;
+    const actionForPlayer = allActions[userActionIdx];
 
-    const actionArgs: any = {};
-    const [actionIdxStr, optionIdxStr] = userAction.split(' ');
-    const actionIdx = Number(actionIdxStr);
-    const optionIdx = optionIdxStr ? Number(optionIdxStr) : null;
-    const resultingAction = allActions[actionIdx];
 
-    const requiresChoice = !!(resultingAction?.action as PromptAction).candidateIds;
-    const madeChoice = typeof optionIdx === 'number';
-
-    // If the action needs a choice and you didn't give one
-    if (requiresChoice && !optionIdxStr) {
-      console.error('You need to choose an option');
-      continue;
-    }
-    // If the optionIdx from your choice is invalid
-    if (
-      madeChoice
-      && requiresChoice
-      && optionIdx > (resultingAction?.action as PromptAction).candidateIds!.length
-    ) {
-      console.error('Your choice is invalid');
-      continue;
+    if ((actionForPlayer?.action as PromptAction).candidateIds?.length) {
+      userCandidateId = await select({
+        message: 'Make your selection',
+        choices: (actionForPlayer?.action as PromptAction).candidateIds!.map(id => ({
+          name: id,
+          value: id,
+        })),
+      });
     }
 
-    actionArgs.playerId = resultingAction?.pid,
-    actionArgs.actionId = resultingAction?.action.id,
-    actionArgs.result = (madeChoice && requiresChoice) ? (resultingAction?.action as PromptAction).candidateIds![optionIdx] : null,
+    const actionArgs = {
+      playerId: actionForPlayer?.pid,
+      actionId: actionForPlayer?.action.id,
+      result: userCandidateId,
+    };
 
-    testGame = requestHandler({
-      action: resultingAction?.action.type!,
-      prevGame: testGame,
+    game = requestHandler({
+      action: actionForPlayer?.action.type!,
+      prevGame: game,
       actionArgs,
       loggers: testLoggers
     }).game;
+
     continue;
   }
 }
 
-main();
+// gameLoop();
+createGame();
