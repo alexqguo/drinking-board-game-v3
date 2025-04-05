52da6ae13459386ec8e9a9151291a72a1d7e0f26
Alex Guo <alexqguo@gmail.com>
2025-04-05 13:53:48 -0400

    Fix weird casing issue

---
+++ packages/engine/src/gamestate/TurnCheck.ts
@@ -0,0 +1,16 @@
+import { Context } from '../context.js';
+import { GameState, GameStateHandlerFactory } from './gamestate.types.js';
+import { findGameStateHandler } from './index.js';
+
+export const TurnCheck: GameStateHandlerFactory = (ctx: Context) => ({
+  execute: () => {
+    const currentPlayer = ctx.currentPlayer;
+
+    if (currentPlayer?.hasWon) {
+      return findGameStateHandler(ctx, GameState.TurnEnd).execute();
+    }
+
+    return findGameStateHandler(ctx, GameState.ZoneCheck).execute();
+  },
+  gameState: GameState.TurnCheck
+});
---
+++ packages/engine/src/gamestate/ZoneCheck.ts
@@ -0,0 +1,29 @@
+
+import { ZoneType } from '../boards/boards.types.js';
+import { Context } from '../context.js';
+import { findRuleHandler } from '../rules/index.js';
+import { GameState, GameStateHandlerFactory } from './gamestate.types.js';
+import { findGameStateHandler } from './index.js';
+
+export const ZoneCheck: GameStateHandlerFactory = (ctx: Context) => ({
+  gameState: GameState.ZoneCheck,
+  execute: () => {
+    const { tiles, zones } = ctx.boardHelper.module.board;
+    const currentTile = tiles[ctx.currentPlayer.tileIndex];
+    const currentZone = ctx.boardHelper.zonesById.get(currentTile?.zoneId ?? '');
+
+    if (currentZone?.rule && currentZone.type === ZoneType.active) {
+      const ruleHandler = findRuleHandler(ctx, currentZone.rule);
+
+      ctx.update_setGamePrompt({
+        ruleId: currentZone.rule.id,
+        nextGameState: GameState.TurnStart,
+      });
+
+      ruleHandler.execute();
+      return;
+    }
+
+    return findGameStateHandler(ctx, GameState.TurnStart).execute();
+  }
+});
