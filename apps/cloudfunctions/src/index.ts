de890f96bbcdfcccf54856268087e72a2f859f51
Alex Guo <alexqguo@gmail.com>
2025-04-05 13:09:23 -0400

    add args to cloud func and attempt deploying

--- apps/cloudfunctions/src/index.ts
+++ apps/cloudfunctions/src/index.ts
@@ -1,28 +1,34 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+import { ActionType, requestHandler } from '@repo/engine';
 import { logger } from 'firebase-functions';
 import { onRequest } from 'firebase-functions/v2/https';
 
-import { ActionType, BoardName, requestHandler } from '@repo/engine';
+export const gameRequest = onRequest(async (req, res) => {
+  const { action: actionParam, actionArgs: actionArgsParam } = req.query;
 
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-export const addMessage = onRequest(async (req, res) => {
-  logger.info('Hello world');
-  // Send back a message that we've successfully written the message
-  res.json({result: `Query: ${JSON.stringify(req.query)}`});
-});
+  const action = String(actionParam) as ActionType;
+  const actionArgs = JSON.parse(actionArgsParam as string ?? '{}');
 
-export const createGame = onRequest(async (req, res) => {
-  const args = req.query;
-  const result = requestHandler({
-    action: ActionType.gameCreate,
-    actionArgs: {
-      playerNames: ['P1', 'P2'],
-      board: BoardName.PokemonGen1
-    },
-    prevGame: null,
-    // loggers: testLoggers
-  });
+  try {
+    const result = requestHandler({
+      action,
+      actionArgs,
+      prevGame: null,
+      loggers: {
+        display: logger.debug,
+        debug: logger.debug,
+        error: logger.error,
+      }
+    });
 
-  res.json(result);
+    res.json({
+      success: true,
+      result
+    });
+  } catch (e) {
+    logger.error(e);
+    res.json({
+      success: false,
+      error: JSON.stringify(e)
+    });
+  }
 });
