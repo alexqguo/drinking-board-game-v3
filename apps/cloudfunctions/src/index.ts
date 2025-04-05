5d26b94189b3065b5727c5dc2ba200f1e063095f
Alex Guo <alexqguo@gmail.com>
2025-04-05 12:41:27 -0400

    cf can invoke engine

--- apps/cloudfunctions/src/index.ts
+++ apps/cloudfunctions/src/index.ts
@@ -2,10 +2,27 @@
 import { logger } from 'firebase-functions';
 import { onRequest } from 'firebase-functions/v2/https';
 
+import { ActionType, BoardName, requestHandler } from '@repo/engine';
+
 // Take the text parameter passed to this HTTP endpoint and insert it into
 // Firestore under the path /messages/:documentId/original
 export const addMessage = onRequest(async (req, res) => {
   logger.info('Hello world');
   // Send back a message that we've successfully written the message
   res.json({result: `Query: ${JSON.stringify(req.query)}`});
+});
+
+export const createGame = onRequest(async (req, res) => {
+  const args = req.query;
+  const result = requestHandler({
+    action: ActionType.gameCreate,
+    actionArgs: {
+      playerNames: ['P1', 'P2'],
+      board: BoardName.PokemonGen1
+    },
+    prevGame: null,
+    // loggers: testLoggers
+  });
+
+  res.json(result);
 });
