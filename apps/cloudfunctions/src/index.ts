406899d344c2e458fc0b4f7033d3be27a8011254
Alex Guo <alexqguo@gmail.com>
2025-04-05 22:57:14 -0400

    add cors in cloud func

--- apps/cloudfunctions/src/index.ts
+++ apps/cloudfunctions/src/index.ts
@@ -2,34 +2,36 @@ import { ActionType, requestHandler } from '@repo/engine';
 import { logger } from 'firebase-functions';
 import { onRequest } from 'firebase-functions/v2/https';
 
-export const gameRequest = onRequest(async (req, res) => {
-  const { action: actionParam, actionArgs: actionArgsParam } = req.query;
+export const gameRequest = onRequest(
+  { cors: ['*.alexguo.co', 'localhost:5173'] },
+  async (req, res) => {
+    const { action: actionParam, actionArgs: actionArgsParam } = req.query;
 
-  const action = String(actionParam) as ActionType;
-  const actionArgs = JSON.parse(actionArgsParam as string ?? '{}');
+    const action = String(actionParam) as ActionType;
+    const actionArgs = JSON.parse(actionArgsParam as string ?? '{}');
 
-  try {
-    logger.info(`Invoking engine with args: ${JSON.stringify(req.query)}`);
-    const result = requestHandler({
-      action,
-      actionArgs,
-      prevGame: null,
-      loggers: {
-        display: logger.debug,
-        debug: logger.debug,
-        error: logger.error,
-      }
-    });
+    try {
+      logger.info(`Invoking engine with args: ${JSON.stringify(req.query)}`);
+      const result = requestHandler({
+        action,
+        actionArgs,
+        prevGame: null,
+        loggers: {
+          display: logger.debug,
+          debug: logger.debug,
+          error: logger.error,
+        }
+      });
 
-    res.json({
-      success: true,
-      result
-    });
-  } catch (e) {
-    logger.error(e);
-    res.json({
-      success: false,
-      error: JSON.stringify(e)
-    });
-  }
+      res.json({
+        success: true,
+        result
+      });
+    } catch (e) {
+      logger.error(e);
+      res.json({
+        success: false,
+        error: JSON.stringify(e)
+      });
+    }
 });
