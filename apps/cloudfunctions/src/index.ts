4e38b7eaf975ee6c85ed47033362810a0856f3b9
Alex Guo <alexqguo@gmail.com>
2025-04-06 15:04:05 -0400

    Update cloud function to use transactions and implement display messages

--- apps/cloudfunctions/src/index.ts
+++ apps/cloudfunctions/src/index.ts
@@ -1,15 +1,23 @@
-import { ActionType, requestHandler } from '@repo/engine';
+import { ActionType, Game, requestHandler } from '@repo/engine';
 import { getApps, initializeApp } from 'firebase-admin/app';
 import { getDatabase } from 'firebase-admin/database';
 import { logger } from 'firebase-functions';
 import { HttpsError, onCall } from 'firebase-functions/v2/https';
 
+interface DisplayMessage {
+  msg: string
+}
+
+interface RealtimeDbObject {
+  game: Game,
+  messages: DisplayMessage[],
+}
+
+
 if (!getApps().length) {
   initializeApp();
 }
-// TODO - connect to local emulator instead when developing on local
 const db = getDatabase();
-
 // Connect to emulator instance if running locally
 if (process.env.FIREBASE_DATABASE_EMULATOR_HOST) {
   db.useEmulator(
@@ -18,11 +26,25 @@ if (process.env.FIREBASE_DATABASE_EMULATOR_HOST) {
   )
 }
 
+const getDatabasePath = (id: string) => `games/${id}`;
+const getEngineLoggers = ({
+  displayMessages
+}: { displayMessages: DisplayMessage[] }) => ({
+  display: (s: string) => displayMessages.push({ msg: s }),
+  debug: logger.debug,
+  error: logger.error,
+});
+
 /**
- * notes
- * - deployment script and weirdness, no monorepo support
- * - oncall vs onrequest
- * - invoker role in google cloud console (allUsers)
+ * Cloud function responsible for:
+ * - Handling frontend requests
+ * - Executing game engine
+ * - Updating Realtime DB, using transactions to prevent race conditions
+ *
+ * Realtime DB is structured as:
+ * - /games/{gameId}
+ *  - /game -> engineResponse.game object
+ *  - /messages -> game messages added through the logger
  */
 export const gameRequest = onCall(
   { cors: ['https://*.alexguo.co', 'http://localhost:5173'] },
@@ -32,27 +54,60 @@ export const gameRequest = onCall(
         throw new HttpsError('unauthenticated', 'Not signed in');
       }
 
-      const { action: actionParam, actionArgs: actionArgsParam } = req.data;
+      const {
+        gameId: gameIdParam,
+        action: actionParam,
+        actionArgs: actionArgsParam,
+      } = req.data;
       const action = String(actionParam) as ActionType;
       logger.info(`Invoking engine with args: ${JSON.stringify(req.data)}`);
 
-      const result = requestHandler({
-        action,
-        actionArgs: actionArgsParam,
-        prevGame: null,
-        loggers: {
-          display: logger.debug,
-          debug: logger.debug,
-          error: logger.error,
-        }
-      });
+      if (action === ActionType.gameCreate) {
+        const displayMessages: DisplayMessage[] = [];
+        // If creating a game, we do not need a transaction as no data yet exists
+        const result = requestHandler({
+          action,
+          actionArgs: actionArgsParam,
+          prevGame: null,
+          loggers: getEngineLoggers({ displayMessages }),
+        });
 
-      await db.ref('game/1234').set(result);
+        // Create the game and put it into the database
+        await db.ref(getDatabasePath(result.game.metadata.id)).set({
+          game: result.game,
+          messages: displayMessages
+        });
+      } else {
+        // If updating a game, we need to setup a transaction
+        const ref = db.ref(getDatabasePath(gameIdParam));
 
-      return({
-        success: true,
-        result
-      });
+        // Fetch the current game at that path and use that to update
+        // Ensures use of the latest game version and prevents update race conditions
+        await ref.transaction((currentData: RealtimeDbObject) => {
+          if (!currentData) {
+            throw new Error('Tried to access a gameId that does not exist.');
+          }
+
+          const displayMessages = [...(currentData.messages || [])];
+          const currentGame = currentData.game as Game;
+
+          const result = requestHandler({
+            action,
+            actionArgs: actionArgsParam,
+            prevGame: currentGame,
+            loggers: getEngineLoggers({ displayMessages }),
+          });
+
+          // This is what gets updated into the ref
+          return {
+            game: result.game,
+            messages: displayMessages,
+          };
+        });
+      }
+
+      // Caller doesn't need anything else as the updates will come from direct Realtime DB integration
+      return({ success: true });
     } catch (e) {
       logger.error(e);
       return({
