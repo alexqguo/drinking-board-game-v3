ca0fd8db8c3a7f853cb17e29645d9975e4420d91
Alex Guo <alexqguo@gmail.com>
2025-04-25 18:17:03 -0400

    Update message list

--- packages/react-ui/src/components/GameSidebar.tsx
+++ packages/react-ui/src/components/GameSidebar.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { UISize, useUI } from '../context/UIEnvironmentContext';
+import { useScreenSize } from '../hooks/useScreenSize';
 import { MessageList } from './sidebar/MessageList';
 import { StatusContainer } from './sidebar/StatusContainer';
 
+const containerStyles = {
+  s: undefined,
+  l: {
+    flex: '0 0 50%',
+  },
+};
+
 export const GameSidebar: React.FC = () => {
   const ui = useUI();
+  const { screenSize } = useScreenSize();
 
   return (
-    <ui.Col padding={UISize.m}>
-      <StatusContainer />
-      <MessageList />
+    <ui.Col style={{ height: '100%' }} padding={UISize.m}>
+      <ui.Col style={containerStyles[screenSize]}>
+        <StatusContainer />
+      </ui.Col>
+      {screenSize === 'l' && (
+        <ui.Col style={containerStyles[screenSize]}>
+          <MessageList />
+        </ui.Col>
+      )}
     </ui.Col>
   );
 };
--- packages/react-ui/src/components/sidebar/MessageList.tsx
+++ packages/react-ui/src/components/sidebar/MessageList.tsx
@@ -1,9 +1,17 @@
+import { useEffect, useRef } from 'react';
+import { useI18n } from '../../context/LocalizationContext.js';
 import { useMessages } from '../../context/MessagesContext.jsx';
 import { UISize, useUI } from '../../context/UIEnvironmentContext.jsx';
 
 export const MessageList = () => {
-  const { messages, isLoading, error } = useMessages();
   const ui = useUI();
+  const { getMessage } = useI18n();
+  const { messages, isLoading, error } = useMessages();
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
 
   if (isLoading) return <ui.Spinner size={UISize.s} />;
   if (error) return null;
@@ -13,14 +21,26 @@ export const MessageList = () => {
   }
 
   return (
-    <ui.Col marginTop={UISize.m}>
-      <ul>
-        {messages.map((message, index) => (
-          <li key={index}>
-            <ui.Text fontSize={UISize.xs}>{message.msg}</ui.Text>
-          </li>
-        ))}
-      </ul>
+    <ui.Col style={{ height: '100%' }}>
+      <ui.Text>{getMessage('webapp_messages')}</ui.Text>
+
+      <ui.Row
+        style={{
+          overflow: 'auto',
+          height: 0,
+          flexGrow: 1,
+          scrollbarWidth: 'none',
+        }}
+      >
+        <ul>
+          {messages.map((message, index) => (
+            <li key={index}>
+              <ui.Text fontSize={UISize.xs}>{message.msg}</ui.Text>
+            </li>
+          ))}
+          <div ref={messagesEndRef} />
+        </ul>
+      </ui.Row>
     </ui.Col>
   );
 };
