3debea0b246c254a27811a8d8ef529ac787f1583
Alex Guo <alexqguo@gmail.com>
2025-05-14 10:24:27 -0700

    Order players in sidebar

--- packages/react-ui/src/components/sidebar/StatusContainer.tsx
+++ packages/react-ui/src/components/sidebar/StatusContainer.tsx
@@ -1,15 +1,21 @@
+import type { Player } from '@repo/engine';
 import React from 'react';
 import { useCurrentGame, useCurrentPlayers } from '../../context/GameContext';
 import { useUI } from '../../context/UIEnvironmentContext';
 import { useScreenSize } from '../../hooks/useScreenSize';
 import { PlayerStatus } from './PlayerStatus';
 
+const getOrderedPlayers = (players: Player[]) => {
+  return players.sort((playerA, playerB) => playerA.order - playerB.order);
+};
+
 export const StatusContainer: React.FC = () => {
   const ui = useUI();
   const players = useCurrentPlayers();
   const { screenSize } = useScreenSize();
   const currentPlayerId = useCurrentGame((g) => g.metadata.currentPlayerId);
-  const playersToShow = screenSize === 'l' ? Object.values(players) : [players[currentPlayerId]!];
+  const playersToShow =
+    screenSize === 'l' ? getOrderedPlayers(Object.values(players)) : [players[currentPlayerId]!];
 
   return (
     <ui.Col>
