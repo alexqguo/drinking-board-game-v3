319f46e6df50e1a195afdf9748097c1d21edcb71
Alex Guo <alexqguo@gmail.com>
2025-05-16 17:43:51 -0400

    remove zod references

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -1,10 +1,3 @@
-/**
- * Primary export file for schemas package
- */
-// export * from './basic-types.js';
-// export * from './board-schemas.js';
-// export * from './rule-schemas.js';
-
 export * from './legacy-types.js';
 // Export validation functions
 export { validateBoardModule } from './validation.js';
