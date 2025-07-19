c3710f1c7df2a0b4f8b0a2dad71b13a0b5441d41
Alex Guo <alexqguo@gmail.com>
2025-07-19 12:14:53 -0400

    Remove url parameter gate

--- apps/webapp/src/main.tsx
+++ apps/webapp/src/main.tsx
@@ -1,27 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import {
-  getLocalStorage,
-  initializeLocalStorageIfEmpty,
-  setLocalStorage,
-} from './utils/localStorage';
+import { initializeLocalStorageIfEmpty } from './utils/localStorage';
 
 (() => {
   initializeLocalStorageIfEmpty();
-  const urlSearchParams = new URLSearchParams(window.location.search);
-
-  // todo- make this more generic so you can set any LS thing through the URL
-  if (urlSearchParams.get('isDebugMode') === 'true') {
-    setLocalStorage('isDebugMode', 'true');
-  }
-
-  const isDebugMode = getLocalStorage().isDebugMode;
-
-  if (!isDebugMode) {
-    document.location.href = 'https://v2.drink.alexguo.co';
-    return;
-  }
 
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
